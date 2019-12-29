0fce440a6b1eb8e2615c97c3aea85ad00cbe8d79
faisalhakim <malikfaisalhakim@gmail.com>
2019-12-29 18:09:58 +0430

    Commit #6 Picasso, Android

--- app/src/main/java/app/auaf/myhabitbox/DetailActivity.java
+++ app/src/main/java/app/auaf/myhabitbox/DetailActivity.java
@@ -3,12 +3,17 @@ package app.auaf.myhabitbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
 
     TextView tvName;
+    ImageView ivPhoto;
+    String url="https://babblesports.com/wp-content/uploads/2019/09/Lionel-Messi-Norway-Real-Messi-GettyImages-1166108824.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,15 @@ public class DetailActivity extends AppCompatActivity {
 
 
         tvName=findViewById(R.id.tvName);
+        ivPhoto=findViewById(R.id.ivphoto);
+
+
+
+        Picasso.get()
+                .load(url)
+                .placeholder(R.drawable.ic_logo)
+                .error(R.drawable.ic_cancel)
+                .into(ivPhoto);
 
 
         String nameUser=getIntent().getStringExtra("name");
